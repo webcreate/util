2ab02b034924d17871d8d7468b6fff7d4d2589b1
Philipp Scheit <p.scheit@ps-webforge.com>
2013-10-29 18:40:54 +0100

    adjust test to match

--- tests/Webcreate/Util/CliTest.php
+++ tests/Webcreate/Util/CliTest.php
@@ -22,12 +22,24 @@ class CliTest extends PHPUnit_Framework_TestCase
 
     public function prepareProvider()
     {
-        return array(
+        if (defined('PHP_WINDOWS_VERSION_MAJOR')) {
+            return array(
+                array('test', array('-n' => 1), 'test -n "1"'),
+                array('abc', array('-n=' => 1), 'abc -n="1"'),
+                array('abc', array('-n' => true, 'arg2'), 'abc -n "arg2"'),
+                array('abc', array('-n' => false, 'arg2', 'arg3'), 'abc "arg2" "arg3"'),
+                array('echo', array('-x' => 'hello world'), 'echo -x "hello world"'),
+                array('git', array('log' => '%H%n'), 'git log "%H%n"'),
+           );
+
+        } else {
+            return array(
                 array('test', array('-n' => 1), 'test -n \'1\''),
                 array('abc', array('-n=' => 1), 'abc -n=\'1\''),
                 array('abc', array('-n' => true, 'arg2'), 'abc -n \'arg2\''),
                 array('abc', array('-n' => false, 'arg2', 'arg3'), 'abc \'arg2\' \'arg3\''),
                 array('echo', array('-x' => 'hello world'), 'echo -x \'hello world\''),
-        );
+            );
+        }
     }
 }
